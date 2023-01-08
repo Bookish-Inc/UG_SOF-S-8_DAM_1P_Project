8add7154230d80419b7157237d1f0dd33216384f
VanessaR_21 <vanessa.ronquillos@ug.edu.ec>
2023-01-08 01:16:30 -0500

    Home design

--- app/src/main/java/com/example/bookish_bookshop_app/HomeActivity.java
+++ app/src/main/java/com/example/bookish_bookshop_app/HomeActivity.java
@@ -1,18 +1,14 @@
 package com.example.bookish_bookshop_app;
 
-import static android.app.PendingIntent.getActivity;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class LogInActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_home);
     }
 }
